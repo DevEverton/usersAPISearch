eb900638c6726ab6cf7c55267b609ed738e4a1c1
Everton Carneiro <Everton@MacBook-Pro-de-Everton.local>
2020-05-27 00:56:58 -0300

    Implemented users rendering without search for layout purposes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,57 @@
-window.addEventListener("load", () => {
+window.addEventListener("load", () => load());
+
+async function load() {
   const elements = getElements();
-  fetchData();
-});
+  const users = await fetchData();
+  const input = elements.input;
+  const button = elements.button;
+
+  input.focus();
+  button.disabled = true;
+  searchUsers();
+  renderUsers(users);
+}
 
 function getElements() {
-  const inputSearch = document.querySelector(".form-control");
-  const searchButton = document.querySelector(".btn");
-  return [inputSearch, searchButton];
+  const input = document.querySelector(".form-control");
+  const button = document.querySelector(".btn");
+  return { input, button };
 }
 
-function fetchData() {}
-
-function handleButton(button) {
-  input.addEventListener("keyup", () => {
-    console.log(input.value);
+async function fetchData() {
+  const res = await fetch(
+    "https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo"
+  );
+  const json = await res.json();
+  let users = json.results.map((user) => {
+    return {
+      name: `${user.name.first} ${user.name.last}`,
+      picture: user.picture.large,
+      age: user.dob.age,
+      gender: user.gender,
+    };
   });
+  return users;
 }
+
+function searchUsers() {}
+
+function renderUsers(users) {
+  const usersDiv = document.querySelector(".results-list");
+  let usersHTML = "<div>";
+  users.forEach((user) => {
+    const { name, picture, age, gender } = user;
+    const userHTML = `
+        <div id="${gender}" class="user-container">
+            <img src="${picture}">
+            <span>${name}</span>
+            <span>${age} anos</span>
+        </div>
+    `;
+    usersHTML += userHTML;
+    usersDiv.innerHTML = usersHTML;
+  });
+  usersHTML += "</div>";
+}
+
+function renderStats(stats) {}
